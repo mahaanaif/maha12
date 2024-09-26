e92ac2c91e448154822427ee78062be77caaab12
mahaanaif <mahaa.alharbi76@gmail,com>
2024-09-26 17:05:31 +0300

    Initial Commit

---
+++ maha12/maha12App.swift
@@ -0,0 +1,17 @@
+//
+//  maha12App.swift
+//  maha12
+//
+//  Created by MAHA on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct maha12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ maha12Tests/maha12Tests.swift
@@ -0,0 +1,17 @@
+//
+//  maha12Tests.swift
+//  maha12Tests
+//
+//  Created by MAHA on 23/03/1446 AH.
+//
+
+import Testing
+@testable import maha12
+
+struct maha12Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
